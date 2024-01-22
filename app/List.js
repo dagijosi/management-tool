2b997af723bcb3d80b8390a1b82f2cf3724ffe56
dagijosi <dagij@DAGI-PC>
2024-01-22 10:28:07 +0300

    mutiple list

--- app/List.js
+++ app/List.js
@@ -3,10 +3,11 @@ import React from 'react'
 import Cards from './Cards'
 import { Add } from '@mui/icons-material'
 
-const List = () => {
+const List = ({ title }) => {
+
   return (
-    <div className='flex flex-col items-start rounded bg-gray-100 w-80 p-2'>
-      <p className='text-black text-lg not-italic font-semibold capitalize'>To Do</p>
+    <div className='flex flex-col items-start rounded bg-gray-100 w-96 p-2 mr-16'>
+      <p className='text-black text-lg not-italic font-semibold capitalize'>{title}</p>
       <Cards/>
       <button className='bg-transparent w-full hover:bg-slate-300 flex justify-start p-2 text-sm font-semibold mt-4'>
         <Add className='w-5 h-5 mr-2'/>
--- app/ProjectManagementPage.js
+++ app/ProjectManagementPage.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import List from './List'
+import React from "react";
+import List from "./List";
 
 const ProjectManagementPage = () => {
+  const board = [
+    { id: 1, title: "TODO" },
+    { id: 2, title: "IN PROGRESS" },
+    { id: 3, title: "DONE" },
+  ];
   return (
-    <div className='bg-blue-500 h-screen p-4'>
-        <div className='ml-6'>
-      <p className='text-3xl not-italic font-semibold text-white mb-4'>Project Management</p>
-      <List/>
+    <div className="bg-blue-500 h-screen p-4">
+      <div className="ml-6">
+        <p className="text-3xl not-italic font-semibold text-white mb-4">
+          Project Management
+        </p>
+        <div className="flex flex-row">
+          {board.map((item) => (
+            <List key={item.id} title={item.title} />
+          ))}
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default ProjectManagementPage
+export default ProjectManagementPage;
